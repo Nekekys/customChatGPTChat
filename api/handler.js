efd702705e78a2d97612ef86bf710eb73b790460
Nekekys <geri_337@mail.ru>
2023-06-27 14:34:36 +0300

    -

--- api/handler.js
+++ api/handler.js
@@ -1,16 +1,17 @@
 
 const APIKey = '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'
 export default async function handler(request, response) {
-    const data = await fetch('https://api.openai.com/v1/completions',{
+   fetch('https://api.openai.com/v1/completions',{
         method: 'post',
         body: JSON.stringify(request.query.message),
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${APIKey}`
         }
-    })
-    response.status(200).json({
-        body: data,
-        query: data
-    });
+    }).then(res => response.status(200).json({
+       body: res,
+       query: res
+   }))
+
+
 }
