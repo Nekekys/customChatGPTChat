be182ad1096610cca9b12acfeb388466df11482e
Nekekys <geri_337@mail.ru>
2023-06-27 14:58:39 +0300

    _

--- api/handler.js
+++ api/handler.js
@@ -1,10 +1,20 @@
 
 const APIKey = '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'
 export default async function handler(request, response) {
+    const APIBody = {
+        "model": "text-davinci-003",
+        "prompt": request.query.message,
+        "temperature": 0.9,
+        "max_tokens": 600,
+        "top_p": 1,
+        "frequency_penalty": 0.0,
+        "presence_penalty": 0.6,
+        "stop": [" Human:", " AI:"]
+    }
     try{
         const data = await fetch('https://api.openai.com/v1/completions',{
             method: 'post',
-            body: JSON.stringify(request.query.message),
+            body: JSON.stringify(APIBody),
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${APIKey}`
