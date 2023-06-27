02c3f4d386bfee779541489398008ea2b05316b4
Nekekys <geri_337@mail.ru>
2023-06-27 14:31:26 +0300

    -

--- api/handler.js
+++ api/handler.js
@@ -1,7 +1,16 @@
-export default function handler(request, response) {
+
+const APIKey = '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'
+export default async function handler(request, response) {
+    const data = await fetch('https://api.openai.com/v1/completions',{
+        method: 'post',
+        body: JSON.stringify(request.query.message),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${APIKey}`
+        }
+    })
     response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
+        body: data,
+        query: request.query
     });
 }
