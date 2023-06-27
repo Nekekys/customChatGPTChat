19864f68e8de45274180a970c3de1552fd016af8
Nekekys <geri_337@mail.ru>
2023-06-27 14:51:16 +0300

    -

--- api/handler.js
+++ api/handler.js
@@ -1,20 +1,39 @@
 
 const APIKey = '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'
 export default async function handler(request, response) {
-   return fetch('https://api.openai.com/v1/completions',{
-        method: 'post',
-        body: JSON.stringify(request.query.message),
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${APIKey}`
+    try{
+        const data = await fetch('https://api.openai.com/v1/completions',{
+            method: 'post',
+            body: JSON.stringify(request.query.message),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${APIKey}`
+            }
+        })
+
+        return {
+            statusCode: 200,
+            headers: {},
+            body: data,
         }
-    }).then(res => {
-        console.log(res)
-       response.status(200).json({
-           body: res.data,
-           query: res.data.choices[0].text
-       })
-   })
+    }catch (e) {
+        return {
+            statusCode: 400,
+            headers: {},
+            body: 'Error',
+        }
+    }
+
+
+
+
+    // then(res => {
+    //     console.log(res)
+    //     response.status(200).json({
+    //         body: res,
+    //         query: res
+    //     })
+    // })
 
 
 }
